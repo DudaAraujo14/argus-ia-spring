1e3eddfa1ab30f43710dac135fab21a7f8f25b95
DudaAraujo14 <rm560944@fiap.com.br>
2026-05-30 20:26:55 -0300

    chore: cria projeto Spring Boot do assistente IA

---
+++ src/main/java/br/com/argus/ia/ArgusIaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.argus.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusIaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgusIaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/argus/ia/ArgusIaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.argus.ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusIaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
